a54714e403231d865f86dca224c470d893f60609
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-12-27 20:17:52 -0300

    Models

--- counts/admin.py
+++ counts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category
+from .models import Transaction
+
+admin.site.register(Category)
+admin.site.register(Transaction)
 
 # Register your models here.
--- counts/models.py
+++ counts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    dt_insert = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    dt_insert = models.DateTimeField()
+    description = models.CharField(max_length=200)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    note = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
